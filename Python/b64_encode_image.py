d9f43320ffdb56cc9e4258d37f3632b1a15dc01c
Steve Campbell <sdcampbell68@live.com>
2021-08-28 11:44:07 -0400

    Update b64_encode_image.py

--- Python/b64_encode_image.py
+++ Python/b64_encode_image.py
@@ -1,1 +1,26 @@
+import tkinter as tk
+from tkinter import filedialog
+import base64
+import sys
+import pyperclip
 
+root = tk.Tk()
+root.withdraw()
+root.clipboard_clear()
+
+file_path = filedialog.askopenfilename()
+extension = ""
+if file_path.endswith('.jpg'):
+    extension = "jpg"
+elif file_path.endswith('.png'):
+    extension = "png"
+else:
+    print("Incompatible image type, exiting!")
+    sys.exit()
+
+image_description = input("Enter an image description: ")
+
+with open(file_path, "rb") as image_file:
+    encoded_string = base64.b64encode(image_file.read())
+    pyperclip.copy(f"<img src='data:image/{extension};base64,{encoded_string.decode('utf-8')}'>{image_description}</img>")
+    print("Base64 encoded image tag has been copied to the clipboard.")
