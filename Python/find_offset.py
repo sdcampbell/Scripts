66baec298e9b914de26dfa300a20d9a7a64548aa
Steve Campbell <sdcampbell68@live.com>
2021-09-01 15:42:31 -0400

    Update find_offset.py

--- Python/find_offset.py
+++ Python/find_offset.py
@@ -19,7 +19,7 @@ def main():
               Finds the offset from the stack pointer to the return address from a core dump.
               Expects to find a core file in the current working directory
 
-              Usage: findoffset [size]""")
+              Usage: python3 find_offset.py [size]""")
         sys.exit()
     offset = find_ip(cyclic(int(sys.argv[1])))
     
