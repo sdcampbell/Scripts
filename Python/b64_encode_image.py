f8c985559f29507b8db0c1f99e60b252dab1eac1
Steve Campbell <sdcampbell68@live.com>
2022-02-02 08:37:06 -0500

    Update b64_encode_image.py

--- Python/b64_encode_image.py
+++ Python/b64_encode_image.py
@@ -22,8 +22,4 @@ with open(file_path, "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read())
     pyperclip.copy(f"<img src='data:image/{extension};base64,{encoded_string.decode('utf-8')}'>")
     print("Base64 encoded image tag has been copied to the clipboard.")
-
     
-answer = input("Do you want to delete the image? Y/N: ".rstrip())
-if answer == 'y' or 'Y':
-    os.remove(file_path)
